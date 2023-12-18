ba955b4caf827e1890cd88f5e4dd039a73be7917
211098325 maoding <211098325@smail.nju.edu.cn>
2023-12-18 08:33:52 +0800

    init

---
+++ src/main/java/com/example/menubackend/Entity/utils/Step.java
@@ -0,0 +1,14 @@
+package com.example.menubackend.Entity.utils;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Step {
+    private String description;
+
+    private String imageUrl;
+}
